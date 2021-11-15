416a634e5dfd76db70fc27022374a9373dce4edf
Shimona Carvalho <shimona@truss.works>
2021-11-15 12:03:10 -0800

    Added api app for graphql

--- sith_django/app/settings.py
+++ sith_django/app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "api.schema.schema"
+}
