a21359e7540aa6593569167947c18996db2abe53
Shimona Carvalho <shimona@truss.works>
2021-11-15 11:44:41 -0800

    Fixed type issue

--- sith_django/app/settings.py
+++ sith_django/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from typing import List
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-i8+1w$6lqdxr%j+5y&(fli=c)lb5ve@rm$vb%ydkek9m!z@oua
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List[str] = []
 
 
 # Application definition
