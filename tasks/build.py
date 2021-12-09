09e55dc2b1fca00c7ef2f631a5ab3535f731f9d9
Shimona Carvalho <shimona@truss.works>
2021-12-08 17:09:09 -0800

    Added invoke tasks for test and snapshot update

--- tasks/build.py
+++ tasks/build.py
@@ -10,3 +10,22 @@ def mypy(c):
     """
     c.run('mypy tasks')
     c.run('mypy server')
+
+@task
+def test(c):
+    # type: (Context) -> None
+    """
+    Runs all tests available on the server
+    """
+    with c.cd('server/'):
+        c.run('python manage.py test')
+
+
+@task
+def snapshot_update(c):
+    # type: (Context) -> None
+    """
+    Runs all tests and overwrites snapshots for all snapshot tests
+    """
+    with c.cd('server/'):
+        c.run('python manage.py test --snapshot-update')
