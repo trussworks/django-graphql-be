497fe3e8ce6a07bcc4fbab7e415d5836973337cf
Sandy Wright <sandy@truss.works>
2021-12-27 20:04:27 -0600

    Add typing to choices in constants

--- api/constants.py
+++ api/constants.py
@@ -1,4 +1,8 @@
 """Constant values for the API"""
+ChoicesType = list[
+    # (database value, display value)
+    tuple[str, str]
+]
 
 # Color coding for SITH cases; denotes severity
 COLOR_CODE_GRAY = "gray"
@@ -6,8 +10,7 @@ COLOR_CODE_BLUE = "blue"
 COLOR_CODE_PURPLE = "purple"
 COLOR_CODE_BROWN = "brown"
 
-COLOR_CODE_CHOICES = [
-    # (database value, display value)
+COLOR_CODE_CHOICES: ChoicesType = [
     (COLOR_CODE_GRAY, COLOR_CODE_GRAY),
     (COLOR_CODE_BLUE, COLOR_CODE_BLUE),
     (COLOR_CODE_PURPLE, COLOR_CODE_PURPLE),
@@ -19,8 +22,7 @@ STATUS_PRE_INQUIRY = "pre-inquiry"
 STATUS_IN_PROGRESS = "in progress"
 STATUS_INACTIVE = "zero - inactive"
 
-STATUS_CHOICES = [
-    # (database value, display value)
+STATUS_CHOICES: ChoicesType = [
     (STATUS_PRE_INQUIRY, STATUS_PRE_INQUIRY),
     (STATUS_IN_PROGRESS, STATUS_IN_PROGRESS),
     (STATUS_INACTIVE, STATUS_INACTIVE),
