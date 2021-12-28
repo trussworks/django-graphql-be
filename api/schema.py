d59116e8b8dbc0b5fac6d546b2c29f17c05ef3f2
Sandy Wright <sandy@truss.works>
2021-12-27 20:05:41 -0600

    Implement DjangoQueryMixin in API schema

--- api/schema.py
+++ api/schema.py
@@ -1,49 +1,41 @@
-# api/schema.py
+"""GraphQL API schema definition"""
 from typing import Optional
 
 import graphene
 from graphene_django import DjangoObjectType
 from django.db.models import QuerySet
 
+from .query import DjangoQueryMixin
 from .models import Person, Case
 
 
-class PersonType(DjangoObjectType):  # type: ignore[no-any-unimported]
+class PersonType(DjangoQueryMixin, DjangoObjectType):  # type: ignore[no-any-unimported]
     class Meta:
         model = Person
 
+    prefetch_fields = ("cases", "cases_assigned", "cases__analyst", "cases_assigned__subject")
 
-class CaseType(DjangoObjectType):  # type: ignore[no-any-unimported]
+
+class CaseType(DjangoQueryMixin, DjangoObjectType):  # type: ignore[no-any-unimported]
     class Meta:
         model = Case
+
+    select_fields = ("subject", "analyst")
+    prefetch_fields = ("subject__cases", "subject__cases_assigned", "analyst__cases", "analyst__cases_assigned")
 
 
 class Query(graphene.ObjectType):
     hello = graphene.String(default_value="Hello World!")
     all_cases = graphene.List(CaseType)
+    all_people = graphene.List(PersonType)
 
     @staticmethod
     def resolve_all_cases(root: Optional[type], info: graphene.ResolveInfo) -> QuerySet:
-        select_args: list[str] = []
-        prefetch_args: list[str] = []
-        q = Case.objects
+        return CaseType.build_optimized_query(info).all()
 
-        # info.field_asts[0] is our API query name - we would not be here if we didn't have that
-        for field in info.field_asts[0].selection_set.selections:
-            if field.name.value in ["subject", "analyst"]:
-                select_args.append(str(field.name.value))
-
-                if field.selection_set and \
-                        any(subfield.name.value.startswith("cases") for subfield in field.selection_set.selections):
-                    prefetch_args.extend([f"{field.name.value}__cases", f"{field.name.value}__cases_assigned"])
-
-        if any(select_args):
-            q = q.select_related(*select_args)
-
-            if any(prefetch_args):
-                q = q.prefetch_related(*prefetch_args)
-
-        return q.all()
+    @staticmethod
+    def resolve_all_people(root: Optional[type], info: graphene.ResolveInfo) -> QuerySet:
+        return PersonType.build_optimized_query(info).all()
 
 
 schema = graphene.Schema(query=Query)
