72b6fffed8313f05cc829f2ef0169ffdff7f8dfe
Sandy Wright <sandy@truss.works>
2022-01-10 18:15:42 -0600

    Add test for "allPeople" query

--- api/tests/test_schema.py
+++ api/tests/test_schema.py
@@ -37,3 +37,38 @@ def test_all_cases(populate_db: PopulateDbType, client_query: ClientQueryType, s
     content: dict = json.loads(response.content)
     # This line makes a snapshot the first time, then compares against the snapshot
     snapshot.assert_match(content)
+
+
+def test_all_people(  # type: ignore[no-any-unimported]
+        populate_db: PopulateDbType, client_query: ClientQueryType, snapshot: PyTestSnapshotTest) -> None:
+    """Test the resolver for returning all people (Person data) from DB"""
+
+    # Load default test data into the database:
+    populate_db()
+
+    response: HttpResponse = client_query('''
+        query {
+          allPeople {
+            id
+            firstName
+            lastName
+            cases {
+              summary
+              colorCode
+              status
+              receivedAt
+            }
+            casesAssigned {
+              summary
+              colorCode
+              status
+              receivedAt
+            }
+          }
+        }
+        ''')
+    assert response.status_code == 200, f"The response failed with a {response.status_code} status code."
+
+    content: dict = json.loads(response.content)
+    # This line makes a snapshot the first time, then compares against the snapshot
+    snapshot.assert_match(content)
