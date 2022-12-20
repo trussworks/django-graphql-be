716ed06b8bacba7fa03920ac0578cb80effe9c1b
Barry Morrison <bdmorrison@truss.works>
2022-12-20 09:43:50 -0600

    ci: updates pre-commit and formats as a result. This also allows pre-commit to run successfully

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -9,8 +9,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
@@ -32,13 +31,31 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('summary', models.CharField(blank=True, max_length=200, null=True)),
-                ('color_code', models.CharField(blank=True, choices=[('gray', 'gray'), ('blue', 'blue'), ('purple', 'purple'), ('brown', 'brown')], max_length=6, null=True)),
-                ('status', models.CharField(blank=True, choices=[('pre-inquiry', 'pre-inquiry'), ('in progress', 'in progress'), ('zero - inactive', 'zero - inactive')], max_length=20, null=True)),
+                ('color_code',
+                 models.CharField(blank=True,
+                                  choices=[('gray', 'gray'), ('blue', 'blue'), ('purple', 'purple'),
+                                           ('brown', 'brown')],
+                                  max_length=6,
+                                  null=True)),
+                ('status',
+                 models.CharField(blank=True,
+                                  choices=[('pre-inquiry', 'pre-inquiry'), ('in progress', 'in progress'),
+                                           ('zero - inactive', 'zero - inactive')],
+                                  max_length=20,
+                                  null=True)),
                 ('received_at', models.DateTimeField(null=True)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
                 ('updated_at', models.DateTimeField(auto_now=True)),
-                ('analyst', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='incidents_assigned', to='api.person')),
-                ('subject', models.ForeignKey(on_delete=django.db.models.deletion.PROTECT, related_name='incidents', to='api.person')),
+                ('analyst',
+                 models.ForeignKey(blank=True,
+                                   null=True,
+                                   on_delete=django.db.models.deletion.SET_NULL,
+                                   related_name='incidents_assigned',
+                                   to='api.person')),
+                ('subject',
+                 models.ForeignKey(on_delete=django.db.models.deletion.PROTECT,
+                                   related_name='incidents',
+                                   to='api.person')),
             ],
             bases=(api.query.GrapheneQueryMixin, models.Model),
         ),
